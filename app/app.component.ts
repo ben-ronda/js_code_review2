fe01c5429b583554555c439dc646f9b2fdec9082
Ben Ronda <bronda95@comcast.net>
2016-03-27 21:27:58 -0700

    fixed issue when running gulp build and add properties to AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from 'angular2/core';
 
-@Component)({
-  selector: 'my-app'
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>Food/Calorie Checker</h1>
+  `
 })
+
+export class AppComponent{
+
+}
